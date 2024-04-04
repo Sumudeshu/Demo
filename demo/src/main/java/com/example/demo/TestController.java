bb8882cf15e59f5ab335e790197039b99d49e7ef
Sumudeshu <13051565357@163.com>
2024-04-05 01:19:53 +0900

    test2

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 	
-	@GetMapping("/test")
+	@GetMapping("/test1")
 	public String test1() {
 		return "hello world!";
 	}
 
+	@GetMapping("/test2")
+	public String test2() {
+		return "hello world!";
+	}
 }
